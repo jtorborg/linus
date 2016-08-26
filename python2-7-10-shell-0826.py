1e7b87437ba8549563473398135b40c036f45cfb
Jacquelyn <jtorborg1227@comcast.net>
2016-08-26 12:26:08 -0500

    idlecommandline

---
+++ python2-7-10-shell-0826.py
@@ -0,0 +1,28 @@
+Python 2.7.10 (default, Oct 23 2015, 19:19:21) 
+[GCC 4.2.1 Compatible Apple LLVM 7.0.0 (clang-700.0.59.5)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+Visit http://www.python.org/download/mac/tcltk/ for current information.5+5
+SyntaxError: invalid syntax
+>>> 3 +3
+6
+>>> print(hello)
+
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    print(hello)
+NameError: name 'hello' is not defined
+>>> print("hello")
+hello
+>>> 3+3
+6
+>>> to + day
+
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    to + day
+NameError: name 'to' is not defined
+>>> "to" + "day"
+'today'
+>>> 
