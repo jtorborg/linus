7f8d1ee31c50690311fe338462cd7283d6fbb358
Jacquelyn <jtorborg1227@comcast.net>
2016-08-27 19:38:13 -0500

    added automate file

---
+++ automate.py
@@ -0,0 +1,47 @@
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 26 2016, 10:47:25) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+spam = 40
+>>> spam
+40
+>>> eggs = 2
+>>> spam + eggs
+42
+>>> spam + eggs + spam
+82
+>>> spam = spam +2
+>>> spam
+42
+>>> 
+>>> 
+>>> 
+>>> 
+>>> spam = "Hello"
+>>> spam
+'Hello'
+>>> spam = "goodbye"
+>>> spam
+'goodbye'
+>>> ####comments go here######
+>>> ##This program says hello and asks for my name
+>>> print("Hello world!")
+Hello world!
+>>> print("What is your name?) #ask for their name
+      
+SyntaxError: EOL while scanning string literal
+>>> print("What is your name?") #ask for their name
+What is your name?
+>>> myName = input()
+print("It is good t omeet you, ' +myName)
+>>> print("The length of your name:")
+The length of your name:
+>>> print(len(myName))
+41
+>>> ##page 21 of automate book
+>>> print("What is your age?")  #ask for their age
+What is your age?
+>>> myAge = input()
+print("You will be " + str(int(myAge) + 1) + ' in a year.')
+>>> 
